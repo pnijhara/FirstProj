e3ae893af37f6ece49da3a1e6b8df6a5def2d18e
pnijhara <prajjwalnijhara@gmail.com>
2019-01-07 14:02:46 +0530

    First commit

---
+++ polls/views.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.core.urlresolvers import reverse
+
+from django.template import loader, RequestContext
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse("This is the detailed view of the question: %s" % question_id)
+
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {'question':question, 'error_message': "Please select a choice"})
+    else:
+        selected_choice.votes += 1
+    	selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
